3d0b344de6c942a863e77fcc628230bfd704d8ac
ferxcxcba-cmd <fer.xcx.cba@gmail.com>
2026-02-03 14:15:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
-import os
-import subprocess
-import sys
-
-# ESTO DEBE IR ANTES QUE CUALQUIER OTRO IMPORT
-try:
-    import plotly.express as px
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "plotly"])
-    import plotly.express as px
-
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 from datetime import datetime
 
 # Configuración de página
@@ -128,3 +118,4 @@ elif menu == "Añadir Registro":
             st.balloons() # Animación tecnológica de éxito
 
             st.success("Guardado correctamente")
+
